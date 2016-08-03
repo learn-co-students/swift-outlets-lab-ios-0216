02e6bd7b374dabdf6574273c416866104f2c74c2
bhtran <bhtran00@gmail.com>
2016-08-03 16:42:24 -0400

    Completion of lab using switch and IBOutlet

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -10,15 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBAction func redButton(sender: AnyObject) {
+        simonSaysGame.guessRed()
+        complete()
+        print("Red has been tapped")
+    }
+    
+    @IBAction func greenButton(sender: AnyObject) {
+        simonSaysGame.guessGreen()
+        complete()
+        print("Green has been tapped")
+    }
+    
+    @IBAction func yellowButton(sender: AnyObject) {
+        simonSaysGame.guessYellow()
+        complete()
+        print("Yellow has been tapped")
+    }
+    
+    @IBAction func blueButton(sender: AnyObject) {
+        simonSaysGame.guessBlue()
+        complete()
+        print("Blue has been tapped")
+    }
+    
     @IBOutlet weak var displayColorView: UIView!
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var winLabel: UILabel!
+    
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        winLabel.hidden = true
+        
     }
+
 }
 
 // MARK: - SimonSays Game Methods
@@ -33,6 +61,7 @@ extension ViewController {
     }
     
     private func displayTheColors() {
+        
         self.view.userInteractionEnabled = false
         UIView.transitionWithView(displayColorView, duration: 1.5, options: .TransitionCurlUp, animations: {
             self.displayColorView.backgroundColor = self.simonSaysGame.nextColor()?.colorToDisplay
@@ -46,5 +75,38 @@ extension ViewController {
                     print("Pattern to match: \(self.simonSaysGame.patternToMatch)")
                 }
         })
+    }
+    
+    func complete() {
+        
+        buttonsClicked = buttonsClicked + 1
+        
+        switch buttonsClicked {
+        case 5:
+            
+            switch simonSaysGame.wonGame() {
+            case true:
+                winLabel.hidden = false
+                winLabel.text = "You win!"
+            default:
+                winLabel.hidden = false
+                winLabel.text = "You lost!"
+            }
+            
+        case 6:
+            
+            buttonsClicked = 0
+            winLabel.hidden = true
+            winLabel.text = ""
+            self.startGameButton.hidden = false
+            
+        default:
+            
+            winLabel.text = "Keep playing"
+            
+        }
+        
+        
+        
     }
 }
